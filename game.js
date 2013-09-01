6fc39f61858363646c4cf2c4244102588da2ac52
hps <hp@sarstrom.se>
2013-09-02 00:24:34 +0200

    Getting close to bedtime

--- game.js
+++ game.js
@@ -229,7 +229,7 @@ function writeText(string, x, y, color) {
 				HALFTILE);
 			if(color) {
 				map = ctx.getImageData(x+(xOff*HALFTILE)+HALFTILE, y, HALFTILE, HALFTILE);
-				imdata = map,data;
+				imdata = map.data;
 				for(var p = 0, len = imdata.length; p < len; p+=4) {
 					r = imdata[p]
 					g = imdata[p+1];

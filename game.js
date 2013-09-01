c929b75d34b46a65cfcc4dd5d10611072efec850
hps <hp@sarstrom.se>
2013-09-02 00:36:47 +0200

    .

--- game.js
+++ game.js
@@ -235,7 +235,7 @@ function writeText(string, x, y, color) {
 					g = imdata[p+1];
 					b = imdata[p+2];
 
-					if(r==252 & g==252 && b == 252) {
+					if(r==252 && g==252 && b == 252) {
 						imdata[p] = color[0];
 						imdata[p+1] = color[1];
 						imdata[p+2] = color[2];
@@ -262,3 +262,7 @@ function animate() {
 
 	window.requestAnimationFrame(animate);
 }
+
+			ctx.drawImage(img, (23*TILESIZE)+(char[0]*HALFTILE),  font['m'][1]*HALFTILE,  HALFTILE,  HALFTILE,  0, 0, HALFTILE, 	HALFTILE);
+			map = ctx.getImageData(0, 0, HALFTILE, HALFTILE);
+
